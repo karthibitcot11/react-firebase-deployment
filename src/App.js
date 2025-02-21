796fa159c3b6a8594e5660e03f71c26ed6716e22
karthibitcot11 <karthikeyanm01196@gmail.com>
2025-02-22 03:28:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to shathi infotech!!!
+          Welcome to shathi infotech!!
         </p>
         <a
           className="App-link"
