f1805058f25d9ceb4ee90c7719fe43a5f814727b
karthibitcot11 <karthikeyanm01196@gmail.com>
2025-02-22 02:19:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellow world! from feature branch 
+          Hellow world! 
         </p>
         <a
           className="App-link"
